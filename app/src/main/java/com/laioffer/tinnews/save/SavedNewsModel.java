75a6b0384d318ffa5043f474a79457bffae73b08
zhouweikai <zhouweikaier@gmail.com>
2019-04-04 01:20:23 -0700

    First time upload a former Android News App

---
+++ app/src/main/java/com/laioffer/tinnews/save/SavedNewsModel.java
@@ -0,0 +1,33 @@
+package com.laioffer.tinnews.save;
+
+import com.laioffer.tinnews.TinApplication;
+import com.laioffer.tinnews.database.AppDatabase;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class SavedNewsModel implements SavedNewsContract.Model {
+    private SavedNewsContract.Presenter presenter;
+    private final AppDatabase db;
+
+    SavedNewsModel() {
+        db = TinApplication.getDataBase();
+    }
+
+
+    @Override
+    public void setPresenter(SavedNewsContract.Presenter presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void fetchData() {
+        db.newsDao().getAll().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(presenter::loadSavedNews, error -> {
+                    System.out.println("error");
+                }, () -> {
+                    System.out.println("complete");
+                });
+    }
+
+}
